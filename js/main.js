313d0bf93a0e649f4f29ff623aa70f7c4c5f4331
Matvey Bouhartsev <programmer@bouhartsev.site>
2021-01-16 20:52:45 +0300

    All before UI

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 error=false;
 
+$(window).on('error', function () {
+	error=true;
+	$('.load').find('p').show();
+	$('.load').find('p').text('Ошибка загрузки');
+	$('.load').find('p').css('left', `${75 - $('.circle_box>p').width()/2}px`);
+});
+
 $(document).ready(function(){
 	$('.load').find('p').on('DOMSubtreeModified', function(){
 		$('.load').find('p').css('left', `${75 - $('.circle_box>p').width()/2}px`);
@@ -16,15 +23,39 @@ $(document).ready(function(){
 				$('.load').find('p').text('Долгая загрузка...');
 			}
 	}, 1000);
+
+	$("body").css('zoom', ($( window ).width()/1920))
+	$( window ).resize(function() {$("body").css('zoom',($( window ).width()/1920))});
+
+	var availableTags = [
+	  "Испанский",
+	  "Итальянский",
+	  "Английский",
+	  "Китайский",
+	  "Русский"
+	];
+
+	$("input[name='name']").autocomplete({source: availableTags});
 });
 
 $(window).on('load', function () {
+	child_arr = $('.slider').children().slice(0, -1);
+	child_arr.each(function(i){
+		if (i==1) $(this).find('.slider_image').find('img').css('padding-top', ($(this).find('.slider_image').height() - $(this).find('.slider_image').find('img').height()));
+		if(i>2) $(this).hide();
+	})
+	product_num=''+child_arr.length;
+	if (child_arr.length<10) product_num='0'+child_arr.length;
+	$('.product_number').html('<span>02</span>/'+product_num);
+
+
+
+
 	clearInterval(load_interval);
 	if(error==false){
 		$('.load').fadeOut(1000)
 
-		$("body").css('zoom', ($( window ).width()/1920))
-		$( window ).resize(function() {$("body").css('zoom',($( window ).width()/1920))});
+
 
 		$('#video_promo').click(function() {
 			if (!this.paused) {
@@ -56,25 +87,66 @@ $(window).on('load', function () {
 			$('#video_promo').get(0).play();
 		});
 
-		$('div.controller#forward').click(function(){
-//			child_arr = $('.slider').children().slice(0, -1);
-//			if (child_arr.length>3) {
-//				selection_id = child_arr.find('.product_select').prop('id');
-//				selection_id.replace('product', '');
-				console.log($('.slider').children().find('.product_select'));
-//			}
+		$('div.controller').click(function(event){
+			direction = event.currentTarget.getAttribute('id')
+			child_arr = $('.slider').children().slice(0, -1);
+			if (child_arr.length>3) {
+				selection_product = $('.slider').find('.product_select');
+				selection_id = Number(selection_product.prop('id').replace('product', ''));
+				if((selection_id!=2 && direction=='back')||(selection_id!=child_arr.length-1 && direction=='forward')){
+					if(selection_id!=2 && direction=='back') {
+						selection_product.next().hide();
+
+						selection_product.removeClass('product_select');
+						selection_product.addClass('not_select');
+						selection_product.find('.link').removeClass('button');
+						selection_product.find('.link').addClass('product_link');
+
+						selection_product.prev().removeClass('not_select');
+						selection_product.prev().addClass('product_select');
+						selection_product.prev().find('.link').removeClass('product_link');
+						selection_product.prev().find('.link').addClass('button');
+
+						selection_product.prev().prev().show();
+
+						selection_product.prev().find('.slider_image').find('img').css('padding-top', (selection_product.prev().find('.slider_image').height() - selection_product.prev().find('.slider_image').find('img').height()));
+						selection_product.prev().prev().find('.slider_image').find('img').css('padding-top', 0);
+
+						selection_id--;
+					}
+					else {
+						selection_product.prev().hide();
+
+						selection_product.removeClass('product_select');
+						selection_product.addClass('not_select');
+						selection_product.find('.link').removeClass('button');
+						selection_product.find('.link').addClass('product_link');
+
+						selection_product.next().removeClass('not_select');
+						selection_product.next().addClass('product_select');
+						selection_product.next().find('.link').removeClass('product_link');
+						selection_product.next().find('.link').addClass('button');
+
+						selection_product.next().next().show();
+
+						selection_product.next().find('.slider_image').find('img').css('padding-top', (selection_product.next().find('.slider_image').height() - selection_product.next().find('.slider_image').find('img').height()));
+						selection_product.next().next().find('.slider_image').find('img').css('padding-top', 0);
+
+						selection_id++;
+					}
+					product_num = ''+selection_id;
+					if(selection_id<10) product_num = '0'+selection_id;
+					$('.product_number').find('span').text(product_num);
+
+					selection_product.find('.slider_image').find('img').css('padding-top', 0);
+				}
+			}
 		});
 
 		$('.load').detach();
 	}
 });
 
-$(window).on('error', function () {
-	error=true;
-	$('.load').find('p').show();
-	$('.load').find('p').text('Ошибка загрузки');
-	$('.load').find('p').css('left', `${75 - $('.circle_box>p').width()/2}px`);
-});
-
 //on, show, hide, text, css, detach, prependTo, width, find, ready - 10
-//addClass, removeClass, fadeOut, get, children, prop
+//addClass, removeClass, fadeOut, get, children, prop, prev, next, etc.
+//
